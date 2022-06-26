c6208834477367a8aaf6727f6c732bd8bd5983f6
Jakub Tarkowski <45311300+jtarkowski27@users.noreply.github.com>
2022-06-26 12:14:35 +0200

    Add an experiment with Flame

--- lib/force_network.dart
+++ lib/force_network.dart
@@ -1,7 +1,58 @@
 library force_network;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:math';
+
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class NodeComponent extends PositionComponent {
+  static final Paint _paint = Paint()..color = Colors.red;
+  double radius = 100;
+  double delta = 0;
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+
+    x = 0;
+    y = 100;
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+
+    canvas.drawCircle(Offset(x, y), 50, _paint);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    x = sin(delta) * radius;
+    y = cos(delta) * radius;
+
+    delta += dt;
+  }
+}
+
+class NetworkSimaultion extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    add(NodeComponent());
+  }
+}
+
+class ForceNetwork extends StatelessWidget {
+  const ForceNetwork({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final simulation = NetworkSimaultion();
+
+    return GameWidget(game: simulation);
+  }
 }
